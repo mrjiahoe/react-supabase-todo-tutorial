2a478e146b1c6821eeb2e12eca5cf8d56bf9968d
jiahoe <mrjiahoe@hotmail.com>
2022-01-03 18:32:12 +0800

    add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
-import { Box } from "@chakra-ui/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Navbar from "../components/Navbar";
+import { supabaseClient } from "../lib/client";
 
 const Home = () => {
+	const router = useRouter();
+	const user = supabaseClient.auth.user();
+
+	useEffect(() => {
+		if (!user) {
+			router.push("/signin");
+		}
+	}, [user, router]);
+
 	return (
 		<div>
 			<Head>
 				<title>TodoApp</title>
 				<meta
 					name="description"
-					content="Awesome todoapp to store your awesome todos"
+					content="Awesome todoApp to store your awesome todos"
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
-				<Box>Hello World</Box>
+				<Navbar />
 			</main>
 		</div>
 	);
