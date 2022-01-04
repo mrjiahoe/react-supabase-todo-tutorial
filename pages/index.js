419ef357de12b4f13183476976157748c016c9ff
jiahoe <mrjiahoe@hotmail.com>
2022-01-04 13:12:20 +0800

    show todo

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import { useDisclosure } from "@chakra-ui/hooks";
+import { Box, SimpleGrid, Text, HStack, Tag } from "@chakra-ui/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import ManageTodo from "../components/ManageTodo";
 import Navbar from "../components/Navbar";
+import SingleTodo from "../components/SingleTodo";
 import { supabaseClient } from "../lib/client";
 
 const Home = () => {
 	const initialRef = useRef();
-	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [todos, setTodos] = useState([]);
 
 	const router = useRouter();
+	const { isOpen, onOpen, onClose } = useDisclosure();
 	const user = supabaseClient.auth.user();
 
 	useEffect(() => {
@@ -18,6 +21,39 @@ const Home = () => {
 			router.push("/signin");
 		}
 	}, [user, router]);
+
+	useEffect(() => {
+		if (user) {
+			supabaseClient
+				.from("todos")
+				.select("*")
+				.eq("user_id", user?.id)
+				.order("id", { ascending: false })
+				.then(({ data, error }) => {
+					if (!error) {
+						setTodos(data);
+					}
+				});
+		}
+	});
+
+	useEffect(() => {
+		const todoListener = supabaseClient
+			.from("todos")
+			.on("*", (payload) => {
+				const newTodo = payload.new;
+				setTodos((oldTodos) => {
+					const newTodos = [...oldTodos, newTodo];
+					newTodos.sort((a, b) => b.id - a.id);
+					return newTodos;
+				});
+			})
+			.subscribe();
+
+		return () => {
+			todoListener.unsubscribe();
+		};
+	}, []);
 
 	return (
 		<div>
@@ -32,6 +68,25 @@ const Home = () => {
 			<main>
 				<Navbar onOpen={onOpen} />
 				<ManageTodo isOpen={isOpen} onClose={onClose} initialRef={initialRef} />
+				<HStack m="10" spacing="4" justify="center">
+					<Box>
+						<Tag bg="green.500" borderRadius="3xl" size="sm" mt="1" />
+						Complete
+					</Box>
+					<Box>
+						<Tag bg="yellow.400" borderRadius="3xl" size="sm" mt="1" />{" "}
+						Incomplete
+					</Box>
+				</HStack>
+				<SimpleGrid
+					columns={{ base: 2, md: 3, lg: 4 }}
+					gap={{ base: "4", md: "6", lg: "8" }}
+					m="10"
+				>
+					{todos.map((todo) => (
+						<SingleTodo todo={todo} key={todo.id} />
+					))}
+				</SimpleGrid>
 			</main>
 		</div>
 	);
